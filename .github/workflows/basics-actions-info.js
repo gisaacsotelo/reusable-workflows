6daa3b62cddfee01e9cdacd04ccf076f6d4dd6e5
gisaacsotelo <gisaac.sotelo@gmail.com>
2023-02-27 23:29:55 +1300

    try

--- .github/workflows/basics-actions-info.js
+++ .github/workflows/basics-actions-info.js
@@ -1,11 +1,11 @@
-// ^ VARIABLES
-/* 
-2 ways to define a variable:
-- for a single workflow
+// // ^ VARIABLES
+// /* 
+// 2 ways to define a variable:
+// - for a single workflow
 
 
-ACCESING VARIABLES:
-# $ We can access input data w the ${{ input.[your_input_name] }}
-@ jsdflkjs
-*/
-echo "Like an environmet variable: ${{ jobs.job1.outputs.job1_output }}
+// ACCESING VARIABLES:
+// # $ We can access input data w the ${{ input.[your_input_name] }}
+// @ jsdflkjs
+// */
+// echo "Like an environmet variable: ${{ jobs.job1.outputs.job1_output }}"
